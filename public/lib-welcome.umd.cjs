f40d5bd55dc80070e60f953203209aff4d22df27
PBK-B <PBK-B@PBK6.cn>
2022-12-29 17:37:19 +0800

    feat: support CJS module

---
+++ public/lib-welcome.umd.cjs
@@ -0,0 +1,6 @@
+/*
+ * @Author: Bin
+ * @Date: 2022-12-29
+ * @FilePath: /vue-library-tmp/public/lib-welcome.umd.cjs
+ */
+(function(e,o){typeof exports=="object"&&typeof module<"u"?o(exports):typeof define=="function"&&define.amd?define(["exports"],o):(e=typeof globalThis<"u"?globalThis:e||self,o(e.LibWelcome={}))})(this,function(e){"use strict";function o(t){return`Hello welcome ${t}, this a welcome message.`}e.getWelcomeMessage=o,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})});
